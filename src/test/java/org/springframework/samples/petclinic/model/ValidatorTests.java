52edaaf129e9cdb848120f66e7f4f332d07695a8
jacob <jacob@webteb.com>
2020-12-10 10:35:47 +0200

    karona

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("nasma");
+		person.setFirstName("karona");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
